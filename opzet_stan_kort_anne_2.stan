dd6b2650bb5dc350064a9f800aecf3630f13c43c
Daniel Scott <dnscott87@gmail.com>
2016-02-10 16:00:07 -0800

    More standardization of fmt, naming, commenting.
    Also, changed call-name in testRL_stan2 so rename in next commit will
    work.

--- opzet_stan_kort_anne_2.stan
+++ opzet_stan_kort_anne_2.stan
@@ -6,121 +6,110 @@ data{
   int<lower=0,upper=1  > Reward  [n_t[n_s+1]];        // reward (=1, yes)? trial n_t
   int<lower=1,upper=n_s> Subject [n_t[n_s+1]];        // subject number
   int<lower=0,upper=1  > Init    [n_t[n_s+1]];        // is this first trial of a subject? Should RL be initialized?
-}                                                     // end data
+}
 
 parameters{
-  // group level mean parameters
+  // Parameter means over whole group
 	real mu_b_pr; 				                           //inverse gain parameter
 	real mu_ag_pr;   				                        //alphaG
 	real mu_al_pr;   				                        //alphaL
  
-
-  // group level standard deviation
+  // Parameter standard deviations over whole group
    real<lower=0> sd_b;   		  	                     //inverse gain parameter
 	real<lower=0> sd_ag;   				                  //alphaG
 	real<lower=0> sd_al;   				                  //alphaL
   
-
-  // individual level parameters
-   real b_ind_pr[n_s];   			                     //inverse gain parameter
+  // Parameters for individuals
+   real b_ind_pr [n_s];   			                     //inverse gain parameter
 	real ag_ind_pr[n_s];   				                  //alphaG
 	real al_ind_pr[n_s];   				                  //alphaL
-}//end paramters
+}
 	
 transformed parameters{
-   // group level mean parameters
+  // Parameter means over whole group
    real<lower=0,upper=100> mu_b; 		 		         //inverse gain parameter
-   real<lower=0,upper=1> mu_ag;   				         //alphaG
-   real<lower=0,upper=1> mu_al;   				         //alphaL
+   real<lower=0,upper=1  > mu_ag;   				      //alphaG
+   real<lower=0,upper=1  > mu_al;   				      //alphaL
 
+  // Parameter standard deviations over whole group
+   real<lower=0,upper=100> b_ind [n_s];     		      //inverse gain parameter
+   real<lower=0,upper=1  > ag_ind[n_s];   				//alphaG
+   real<lower=0,upper=1  > al_ind[n_s];   	         //alphaL
 
-   // individual level parameters
-   real<lower=0,upper=100> b_ind[n_s];     		      //inverse gain parameter
-   real<lower=0,upper=1> ag_ind[n_s];   				   //alphaG
-   real<lower=0,upper=1> al_ind[n_s];   				   //alphaL
+  // Parameters for individuals (probit)
+   mu_b  <- Phi(mu_b_pr )*100;   	                  //inverse gain parameter
+   mu_ag <- Phi(mu_ag_pr);   				               //alphaG
+   mu_al <- Phi(mu_al_pr);   				               //alphaL
 
-
-   // group level mean parameters (probit)
-   mu_b  <-Phi(mu_b_pr)*100;   		                  //inverse gain parameter
-   mu_ag <-Phi(mu_ag_pr);   				               //alphaG
-   mu_al <-Phi(mu_al_pr);   				               //alphaL
-
-
-   // individual level parameters (probit)
-   for (s in 1:n_s)
-   {
-      b_ind[s]  <- Phi(b_ind_pr[s])*100;
-      ag_ind[s] <- Phi(ag_ind_pr[s]);
-      al_ind[s] <- Phi(al_ind_pr[s]);
-
-   }// end for loop
-}// end transformed parameters
+  // Parameters for individuals (probit)
+   for (subj in 1:n_s){
+      b_ind [subj] <- Phi(b_ind_pr [subj])*100;
+      ag_ind[subj] <- Phi(ag_ind_pr[subj]);
+      al_ind[subj] <- Phi(al_ind_pr[subj]);
+   }
+}
 	
 model{
-  // define general variables needed for subject loop
-  int si;
-  real prQ0[6];
-  real prQ[6];
-  real Qchoice[2];
-  real epsilon;
-  int a;
-  real alpha;
-  vector[2] pchoice;
+   // define vars needed for subject loop
+   int       sid;                                     // Subject ID
+   real      prQ0[6];                                 //
+   real      prQ [6];                                 //
+   real      Qchoice[2];                              //
+   real      epsilon;                                 //
+   int       success;                                 //
+   real      alpha;                                   //
+   vector[2] pchoice;                                 //
 
-  // set prior on group level mean parameters
-   mu_b_pr  ~ normal(0,1);   			                     //inverse gain parameter
-	mu_ag_pr ~ normal(0,1);   				                  //alphaG
-	mu_al_pr ~ normal(0,1);   				                  //alphaL
- 
+   // set prior on group level mean parameters
+   mu_b_pr  ~ normal(0,1);   			                  //inverse gain parameter
+   mu_ag_pr ~ normal(0,1);   				               //alphaG
+   mu_al_pr ~ normal(0,1);   				               //alphaL
 
-  // set prior on group level standard deviations
-   sd_b  ~ uniform(0,1.5);     		                     //inverse gain parameter
-	sd_ag ~ uniform(0,1.5);   				                  //alphaG
-	sd_al ~ uniform(0,1.5);   				                  //alphaL
-  
+   // set prior on group level standard deviations
+   sd_b  ~ uniform(0,1.5);     		                  //inverse gain parameter
+   sd_ag ~ uniform(0,1.5);   				               //alphaG
+   sd_al ~ uniform(0,1.5);   				               //alphaL
 
-  // set prior for individual level parameters
-  for (s in 1:n_s)
-  {
-   b_ind_pr[s]  ~ normal(mu_b_pr ,   sd_b);     		   //inverse gain parameter
-   ag_ind_pr[s] ~ normal(mu_ag_pr,  sd_ag);   				//alphaG
-   al_ind_pr[s] ~ normal(mu_al_pr,  sd_al);   				//alphaL
+   // set prior for individual level parameters
+   for (subj in 1:n_s){
+      b_ind_pr [subj] ~ normal(mu_b_pr ,  sd_b);     	//inverse gain parameter
+      ag_ind_pr[subj] ~ normal(mu_ag_pr, sd_ag);      //alphaG
+      al_ind_pr[subj] ~ normal(mu_al_pr, sd_al);   	//alphaL
+   }
 
-  }
-
-  // define epsilon
-  epsilon <- 0.00001;
+   // Define epsilon (?) why this value
+   epsilon <- 0.00001;
 
   // now start looping over subjects
-  for (t in 1:n_t[n_s+1])
-  {
+   for (trial in 1:n_t[n_s+1]){
 
-      // set initial values subject
-      if (Init[t]==1){
-         si <- Subject[t];
+      // set initial values (i.e. values for trial 1) for the subject
+      if (Init[trial] == 1){
+         si <- Subject[trial];                              // Alias the subject id
          for (v in 1:6) {
-             prQ0[v] <- 0.5;
-             prQ[v]  <- 0.5;
-         }// end inital values loop
-       
-       // trial 1
-       pchoice[1] <- 0.5;
-       pchoice[2] <- 0.5;
+            prQ0[v] <- 0.5;                                 // (?)
+            prQ [v] <- 0.5;                                 // (?)
+         }
+         pchoice[1] <- 0.5;                                 // Prob. of picking choice 1 on trial 1
+         pchoice[2] <- 0.5;                                 // Prob. of picking choice 2 on trial 1
       }
 
-      Qchoice[1] <- prQ[Choice[t]]; 
-      Qchoice[2] <- prQ[(Choice[t]+1)];
-      pchoice[1] <- 1/(1+exp(b_ind[si]*(Qchoice[2]-Qchoice[1])));
-      pchoice[2] <- 1-pchoice[1];
-      pchoice[1] <- epsilon/2+(1-epsilon)*pchoice[1];
-      pchoice[2] <- epsilon/2+(1-epsilon)*pchoice[2];
+      Qchoice[1] <- prQ[Choice[trial]  ];                   // (?)
+      Qchoice[2] <- prQ[Choice[trial]+1];                   // (?)
 
-      Correct[t] ~ categorical(pchoice);
-      a <- Correct[t]-1; //0=correct,1=incorrect	
+      pre_squash <- b_ind[si]*(Qchoice[2] - Qchoice[1]);
+      pchoice[1] <- 1 / ( 1 + exp(pre_squash) );            // Probability of picking choice 1
+      pchoice[2] <- 1 - pchoice[1];                         // Probability of picking choice 2
+
+      pchoice[1] <- epsilon/2+(1-epsilon)*pchoice[1];       // (?)
+      pchoice[2] <- epsilon/2+(1-epsilon)*pchoice[2];       // (?)
+
+      Correct[trial] ~ categorical(pchoice);
+      success <- Correct[trial] - 1;                        // Success of choice, boolean	
+      index   <- Choice[trial] + success;                   // (?)
 
       // reinforcement
-      alpha <- Reward[t]*ag_ind[si]+(1-Reward[t])*al_ind[si];
-      prQ[(Choice[t]+a)] <- prQ[(Choice[t]+a)] + alpha*(Reward[t]-prQ[(Choice[t]+a)]);
-
-   }// end subject loop
-}// end of model loop
+      alpha      <- Reward[trial] *ag_ind[si] + (1-Reward[trial]) *al_ind[si];
+      prQ[index] <- prQ[index] + alpha *(Reward[trial] - prQ[index]);
+   }
+}
--- testRL_stan2.m
+++ testRL_stan2.m
@@ -1,60 +1,93 @@
-function [] = testRL_stan2()
+function [] = test_STAN_flat_RL_recovery()
+% [] = test_STAN_flat_RL_recovery simulates human experimental data for a stacked one-step 
+% selection paradigm, i.e. presentation of AB, CD, EF pairs for which each choice (e.g. A in AB)
+% has a fixed probability of resulting in a reward, and then uses STAN to recover the parameters
+% governing the simulated agents' choices.
 
-%% Simulate Presentation
-alpha_gain = .2;                                            % Alpha gain
-alpha_loss = .1;                                            % Alpha loss
+%% Simulate Human Data for Experimental Paradigm 
+% Stimuli (i.e. AB, CD, EF pairs) are coded 1,3, or 5, with selection reward probabilities coded
+% serially in the variable "reward_prob".
+%
+% Notes:
+%    1. Still uncertain about the beta/softmax math
+%    2. Indexing is all crazy
+%    3. This whole thing can probably be vectorized
+
+alpha_gain = .2;                                            % Importance of gains to agents
+alpha_loss = .1;                                            % Importance of losses to agents
 
 ns    = 24;                                                 % Number of Subjects
-betas = sort(7 + randn(1,25));                              % ?
+betas = sort(7 + randn(1,25));                              % (?) See notes
 
-nt = [repmat([150 300],1,ns/2)];                            % Number of trials each subject sees
-nt = [nt sum(nt)];                                          % is either 150, 300
+trial_counts = repmat([150 300],1,ns/2);                    % Number of trials each subject
+ntrials      = sum(trial_counts);                           % sees, either 150 or 300
+trial_counts = [trial_counts ntrials];                      % Save total for whole-group analy.
 
-stims = repmat([1 3 5],1,50);                               % Stimulus presented, for each 
-stims = stims(randperm(length(stims)));                     % ...
-stims = repmat(stims,2*ns);
+stims = repmat([1 3 5],1,50);                               % Stimulus presented for each
+stims = stims(randperm(length(stims)));                     % trial (randomized)
+stims = repmat(stims,2*ns);                                 % Resultant size 2*ns-by-300*ns
 
-reward_prob = [.8 .2 .7 .3 .6 .4];                          % Probability of being rewarded 
-smfn = @(x) 1/(1 + exp(x));                                 % Define softmax function
+reward_prob = [.8 .2 .7 .3 .6 .4];                          % Reward prob.s for A/B, C/D, E/F
+smfn = @(x) 1./(1 + exp(x));                                % Define softmax squashing function
 
-lin_ind = 0;                                                % Linear index for ns-by-nt matrix
-for subj_ind = 1:ns                                         % 
-   Q = 0.5 * ones(1,6);                                     % ?
+% Pre-initialize large matrices
+outcomes = NaN(1,ntrials);                                  % Outcomes for simulated choices
+rewards  = NaN(1,ntrials);                                  % Whether each outcome is rewarded
+subj_ids = NaN(1,ntrials);                                  % The id. number of each subject
+inits    = NaN(1,ntrials);                                  % Boolean, initialize learning?
 
-   for trial = 1:nt(subj_ind)                               % 
-      lin_ind  = lin_ind + 1;                               % Update counter
-      cur_stim = stims(trial);                              % Current stimulus
-      pair     = Q(cur_stim + [0 1]);                       % (?)
-      softmax  = smfn(betas(subj_ind)*(pair(1) - pair(2))); %  
-      success  = 1+(rand < softmax);                        % Correctness as a logical
-      reward   = rand < reward_prob(cur_stim + (success - 1));     % Gets rewarded? (Boolean) 
-      LR       = alpha_gain *     success ...               % Learning Rate (?)
-               + alpha_loss *(1 - success);                 % Calculate the learning rate
+% Simulate Experiment
+lin_ind = 0;                                                % Linear index for vectors as lists
+for subj_ind = 1:ns                                         % of vars by trial, e.g. "choices"
+   % Initialize prob.s for current agent's choices
+   choice_Ps = 0.5 * ones(1,6);                             % Choice probabilities
 
-      Q(cur_stim + (success - 1)) =      LR  * reward ...
-                                  + (1 - LR) * Q(cur_stim + (success-1));
+   % Simulate this subject's experimental data
+   subj_ntrials = trial_counts(subj_ind);                   % Extract subject's epoch length
+   for trial = 1:subj_ntrials                               % Loop over this subjects trials.
+      lin_ind  = lin_ind + 1;                               % Increment loop counter
+      
+      cur_stim  = stims(trial);                             % Current stimulus
+      stim_inds = cur_stim + [0, 1];                        % Index of choice reward probs.
+      
+      pair     = choice_Ps(stim_inds);                      % Prob. of eg picking A in AB choice
+      pre_sqsh = betas(subj_ind)*(pair(1) - pair(2));       % Pre-squashed 
+      
+      softmax  = smfn(pre_sqsh);                            % (?) See notes
+      success  = 1+(rand < softmax);                        % Correctness as 1 or 2. 1=Corr.
+      
+      rwrd_ind = cur_stim + success - 1;                    % Eg. stim 3 correct => 3, inc. => 4
+      reward   = rand < reward_prob(rwrd_ind);              % Boolean indication of reward 
+      LR       = alpha_gain*reward + alpha_loss*(~reward);  % Calculate the learning rate
 
-      choices  (lin_ind) = cur_stim;
-      outcomes (lin_ind) = success;
-      rewards  (lin_ind) = reward;
-      subj_nums(lin_ind) = subj_ind;
-      inits    (lin_ind) = trial == 1;
+      choice_Ps(rwrd_ind) =      LR *reward  ...            % Update choice probabilities
+                          + (1 - LR)*choice_Ps(rwrd_ind);   % based on outcome of this choice.
+
+      outcomes(lin_ind) = success;                          % Record simulated agent success
+      rewards (lin_ind) = reward;                           % Record feedback given to agent
+      subj_ids(lin_ind) = subj_ind;                         % Record the agent's ID number
+      inits   (lin_ind) = trial == 1;                       % Record if init. must happen
    end
-   disp( ['Q: ' sprintf('%5.3f   ',Q)] )
+   
+   % Display the simulated agent's learned probabilities of making various selections. 
+   disp( ['Choice Probs: ' sprintf('%5.3f   ',choice_Ps)] )
 end
 
 %% Save Stan Input
+%
+% STAN data interface defined as follows:
 % data{
-%   int<lower=1> n_s;                               // number of subjects
-%   int<lower=1> n_t;                               // number of trials for that subject
-%   int<lower=1,upper=5> Choice[n_t ,n_s];          // choice options trial n_t (choice is 1,3 of 5) for subject n_s
-%   int<lower=1,upper=2> Correct[n_t,n_s];          // correct (=1, yes-correct)? trial n_t for subject n_s
-%   int<lower=0,upper=1> Reward[n_t,n_s];           // reward (=1, yes)? trial n_t for subject n_s
+%    int<lower=1> n_s;                       // number of subjects
+%    int<lower=1> n_t;                       // number of trials for that subject
+%    int<lower=1,upper=5> Choice[n_t ,n_s];  // choice options 
+%    int<lower=1,upper=2> Correct[n_t,n_s];  // correct (=1 yes-correct, =2 no-incorrect)
+%    int<lower=0,upper=1> Reward[n_t,n_s];   // reward  (=0 no reward  , =1 yes rewarded)
 % }// end data
 
-RL_data = struct('n_s',ns,'n_t',nt,'Choice',choices,'Correct',outcomes,...
-                 'Reward',rewards,'Subject',subj_nums,'Init',inits);
-%%
+RL_data = struct('n_s',ns,'n_t',trial_counts,'Choice',stims,'Correct',outcomes,...
+                 'Reward',rewards,'Subject',subj_ids,'Init',inits);
+
+%% Perform the STAN fitting
 tic
 fitRL = stan('file','./opzet_stan_kort_anne_2.stan','data',RL_data,'iter',1000,...
              'chains',4,'refresh',100,'warmup',500,'thin',10);
@@ -64,10 +97,13 @@ fitRL.block();% block further instructions
 
 toc
 save fitRL2 fitRL
-%%
+
+%% Show User Fit Outcomes
 print(fitRL)
 fitRL.traceplot
-%%
+
+%% Extract Parameter Fits and Plot Stuff
+
 mu_ag = fitRL.extract('permuted',false).mu_ag;
 mu_al = fitRL.extract('permuted',false).mu_al;
 mu_b  = fitRL.extract('permuted',false).mu_b;
