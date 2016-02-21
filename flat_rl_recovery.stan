6c1112c556a4bd4b83dd95764d663932df139f8f
Daniel Scott <dnscott87@gmail.com>
2016-02-21 14:51:09 -0800

    More tweaks, incl. model interpretation error fix.

--- flat_rl_recovery.stan
+++ flat_rl_recovery.stan
@@ -1,11 +1,11 @@
 data{
   int<lower=1          > n_s;                         // number of subjects
   int<lower=1          > n_t         [n_s+1] ;        // number of trials per subject, plus total number of trials
-  int<lower=1,upper=5  > Choice  [n_t[n_s+1]];        // choice options trial n_t (choice is 1,3 of 5). (All subjects stacked)
-  int<lower=1,upper=2  > Correct [n_t[n_s+1]];        // correct (=1, yes-correct)? trial n_t
-  int<lower=0,upper=1  > Reward  [n_t[n_s+1]];        // reward (=1, yes)? trial n_t
-  int<lower=1,upper=n_s> Subject [n_t[n_s+1]];        // subject number
-  int<lower=0,upper=1  > Init    [n_t[n_s+1]];        // is this first trial of a subject? Should RL be initialized?
+  int<lower=1,upper=5  > stimuli [n_t[n_s+1]];        // 
+  int<lower=1,upper=2  > correct [n_t[n_s+1]];        // correct (=1, yes-correct)? trial n_t
+  int<lower=0,upper=1  > reward  [n_t[n_s+1]];        // reward (=1, yes)? trial n_t
+  int<lower=1,upper=n_s> subject [n_t[n_s+1]];        // subject number
+  int<lower=0,upper=1  > init    [n_t[n_s+1]];        // is this first trial of a subject? Should RL be initialized?
 }
 
 parameters{
@@ -51,16 +51,18 @@ transformed parameters{
 	
 model{
    // Define vars needed for subject loop
-   int       sid;                                     // Subject ID
-   real      action_vals_set [6];                     // A/B, C/D, E/F choice probabilities.
-   real      action_vals_init[6];                     // Initial probabilities.
+   int       sid;                                     // subject ID
+   real      action_vals_set [3,2];                   // A/B, C/D, E/F choice probabilities.
+   real      action_vals_init[3,2];                   // Initial probabilities.
    real      action_vals_cur [2];                     // Choice probabilities for this set, e.g. C/D
    real      epsilon;                                 // 
    int       success;                                 //
    real      alpha;                                   //
    vector[2] pchoice;                                 //
-   real      pre_squash;                              //
+   real      threshold;                               //
    int       index;                                   //
+   int       cur_stim;                                //
+   real      cur_diff;                                //
 
    // Set prior on group level mean parameters
    group_mean_beta       ~ normal(0,1);
@@ -79,40 +81,45 @@ model{
       indiv_alpha_loss[subj] ~ normal(group_mean_alpha_loss, group_sdev_alpha_loss);
    }
 
-   // Set parameter defining the task's underlying probability drift
+   // What is this parameter for?
    epsilon <- 0.00001; 
 
   // now start looping over subjects
    for (trial in 1:n_t[n_s+1]){
 
       // set initial values (i.e. values for trial 1) for the subject
-      if (Init[trial] == 1){
-         sid <- Subject[trial];                                   // Alias the subject id
-         for (v in 1:6) {
-            action_vals_set [v] <- 0.5;
-            action_vals_init[v] <- 0.5;
+      if (init[trial] == 1){
+         sid <- subject[trial];                                   // Alias the subject id
+         for (row in 1:3) {
+            for (col in 1:2) {
+               action_vals_set [row,col] <- 0.5;
+               action_vals_init[row,col] <- 0.5;
+            }
          }
          pchoice[1] <- 0.5;                                       // Prob. of picking choice 1 on trial 1
          pchoice[2] <- 0.5;                                       // Prob. of picking choice 2 on trial 1
       }
 
-      action_vals_cur[1] <- action_vals_set[Choice[trial]  ];
-      action_vals_cur[2] <- action_vals_set[Choice[trial]+1];
-
-      pre_squash <- indiv_beta_tr[sid]*(action_vals_cur[2] - action_vals_cur[1]);
+      cur_stim <- stimuli[trial];
       
-      pchoice[1] <- 1 / ( 1 + exp(pre_squash) );                  // Probability of picking choice 1
+      action_vals_cur[1] <- action_vals_set[cur_stim,1];
+      action_vals_cur[2] <- action_vals_set[cur_stim,2];
+
+      cur_diff   <- action_vals_cur[1] - action_vals_cur[2];
+      threshold  <- indiv_beta_tr[sid]*(cur_diff);
+      
+      pchoice[1] <- 1 / ( 1 + exp(threshold) );                   // Probability of picking choice 1
       pchoice[2] <- 1 - pchoice[1];                               // Probability of picking choice 2
 
       pchoice[1] <- epsilon/2+(1-epsilon)*pchoice[1];             //
       pchoice[2] <- epsilon/2+(1-epsilon)*pchoice[2];             //
 
-      Correct[trial] ~ categorical(pchoice);
-      success <- Correct[trial] - 1;                              // Success of choice, boolean	
-      index   <- Choice[trial] + success;
+      correct[trial] ~ categorical(pchoice);
+      success <- correct[trial];                                 // Success of choice, boolean	
+      index   <- success;
 
       // Reinforcement
-      alpha <- Reward[trial] *indiv_alpha_gain_tr[sid] + (1-Reward[trial]) *indiv_alpha_loss_tr[sid];
-      action_vals_set[index] <- action_vals_set[index] + alpha *(Reward[trial] - action_vals_set[index]);
+      alpha <- reward[trial] *indiv_alpha_gain_tr[sid] + (1-reward[trial]) *indiv_alpha_loss_tr[sid];
+      action_vals_set[cur_stim,index] <- action_vals_set[cur_stim,index] + alpha *(reward[trial] - action_vals_set[cur_stim,index]);
    }
 }
--- test_STAN_flat_RL_recovery.m
+++ test_STAN_flat_RL_recovery.m
@@ -5,18 +5,13 @@ function [] = test_stan_flat_rl_recovery()
 % governing the simulated agents' choices.
 
 %% Simulate Human Data for Experimental Paradigm 
-% Stimuli (i.e. AB, CD, EF pairs) are coded 1,3, or 5, with selection reward probabilities coded
-% serially in the variable "reward_prob".
-%
-% Notes:
-%    1. Indexing is all crazy
-%    2. This whole thing can probably be vectorized
+% Stimuli (i.e. AB, CD, EF pairs) are coded 1,2, or 3.
 
 alpha_gain = .2;                                            % Importance of gains to agents
 alpha_loss = .1;                                            % Importance of losses to agents
 
-ns    = 24;                                                 % Number of Subjects
-betas = sort(7 + randn(1,25));                              % Inverse temp. param ie sensitivity 
+ns    = 2;                                                  % Number of Subjects
+betas = sort(7 + randn(1,ns+1));                            % Inverse temp. param ie sensitivity 
                                                             % ... to Reward Prediction Error
 trial_counts = repmat([150 300],1,ns/2);                    % Number of trials each subject
 ntrials      = sum(trial_counts);                           % ... sees, either 150 or 300
@@ -26,24 +21,27 @@ stims = repmat([1 2 3],1,50);                               % Stimulus presented
 stims = stims(randperm(length(stims)));                     % ... trial (randomized)
 stims = repmat(stims,2*ns);                                 % Resultant size 2*ns-by-300*ns
 
-reward_prob = [0.8,0.2; 0.7,0.3; 0.6,0.4];                  % Reward prob.s for A/B, C/D, E/F
-smfn = @(x) 1./(1 + exp(x));                                % SoftMax FuNction for squashing
-                                                            % ... RPE response
+reward_prob = [0.8, 0.2;                                    % Reward prob.s. Row 1 is for AB
+               0.7, 0.3;                                    % probs for CD
+               0.6, 0.4 ];                                  % probs for EF
+
+smfn = @(x) 1./(1 + exp(x));                                % SoftMax FuNction for squashing ...
+                                                            % RPE response, which 
 % Pre-initialize large matrices
-outcomes = NaN(1,ntrials);                                  % Outcomes for simulated choices
-rewards  = NaN(1,ntrials);                                  % Whether each outcome is rewarded
-subj_ids = NaN(1,ntrials);                                  % The id. number of each subject
-inits    = NaN(1,ntrials);                                  % Boolean, initialize learning?
-choice   = NaN(1,ntrials);                                  % Vector of stimulus presentations
+stanin_correct  = NaN(1,ntrials);                           % Outcomes for simulated choices
+stanin_rewards  = NaN(1,ntrials);                           % Whether each outcome is rewarded
+stanin_subj_ids = NaN(1,ntrials);                           % The id. number of each subject
+stanin_inits    = NaN(1,ntrials);                           % Boolean, initialize learning?
+stanin_stimuli  = NaN(1,ntrials);                           % Vector of stimulus presentations
 
 % Simulate Experiment
 lin_ind = 0;                                                % Linear index for vectors as lists
 for subj_ind = 1:ns                                         % of vars by trial, e.g. "choices"
-   % Initialize prob.s for current agent's choices
-   action_vals = 0.5 * ones(3,2);                             % Choice probabilities
 
    % Simulate this subject's experimental data
+   action_vals  = 0.5 * ones(3,2);                          % Initialize action values
    subj_ntrials = trial_counts(subj_ind);                   % Extract subject's epoch length
+
    for trial = 1:subj_ntrials                               % Loop over this subjects trials.
       lin_ind  = lin_ind + 1;                               % Increment loop counter
       
@@ -52,9 +50,9 @@ for subj_ind = 1:ns                                         % of vars by trial, 
       cur_diff = cur_vals(1) - cur_vals(2);                 %
       
       threshold = smfn(betas(subj_ind)*cur_diff);           % Squashed RPE response
-      success   = rand > threshold;                         % Correctness as boolean
+      success   = rand > threshold;                         % Successfully chose option 1?
        
-      rwrd_ind = success + 1;                               % Col. index into reward prob. matrix
+      rwrd_ind = success + 1;                               % Column index in reward prob. matrix
       reward   = rand < reward_prob(cur_stim,rwrd_ind);     % Boolean indication of reward 
       alpha    = alpha_gain*reward + alpha_loss*(~reward);  % Calculate the learning rate
 
@@ -63,15 +61,16 @@ for subj_ind = 1:ns                                         % of vars by trial, 
 
       action_vals(cur_stim,rwrd_ind) = new_action_vals;     % 
       
-      outcomes(lin_ind) = success;                          % Record simulated agent success
-      rewards (lin_ind) = reward;                           % Record feedback given to agent
-      subj_ids(lin_ind) = subj_ind;                         % Record the agent's ID number
-      inits   (lin_ind) = trial == 1;                       % Record if init. must happen
-      choice  (lin_ind) = cur_stim;                         % Record the current stimulus
+      % Record calculations as vectors, these go to STAN.
+      stanin_correct (lin_ind) = success + 1;               %
+      stanin_rewards (lin_ind) = reward;                    %
+      stanin_subj_ids(lin_ind) = subj_ind;                  %
+      stanin_inits   (lin_ind) = trial == 1;                %
+      stanin_stimuli (lin_ind) = cur_stim;                  %
    end
    
    % Display the simulated agent's learned probabilities of making various selections. 
-   disp( ['Choice Probs: ' sprintf('%5.3f   ',reshape(action_vals',1,6))] )
+   disp( ['Action Vals: ' sprintf('%5.3f   ',reshape(action_vals',1,6))] )
 end
 
 %% Save Stan Input
@@ -84,20 +83,33 @@ end
 %    int<lower=1,upper=2> Correct[n_t,n_s];  // correct (=1 yes-correct, =2 no-incorrect)
 %    int<lower=0,upper=1> Reward[n_t,n_s];   // reward  (=0 no reward  , =1 yes rewarded)
 % }// end data
-
-RL_data = struct('n_s',ns,'n_t',trial_counts,'Choice',choice,'Correct',outcomes,...
-                 'Reward',rewards,'Subject',subj_ids,'Init',inits);
+RL_data = struct('n_s'    ,ns              ...
+                ,'n_t'    ,trial_counts    ...
+                ,'stimuli',stanin_stimuli  ...
+                ,'correct',stanin_correct  ...
+                ,'reward' ,stanin_rewards  ...
+                ,'subject',stanin_subj_ids ...
+                ,'init'   ,stanin_inits    );
+              
+save('./output/sim_data.mat')             
 
 %% Perform the STAN fitting
 tic
-fitRL = stan('file','./flat_rl_recovery.stan','data',RL_data,'iter',1000,...
-             'chains',4,'refresh',100,'warmup',500,'thin',10);
+fitRL = stan('file'       ,'./flat_rl_recovery.stan' ...
+            ,'data'       ,RL_data ...
+            ,'iter'       ,10000   ...
+            ,'chains'     ,4       ...
+            ,'refresh'    ,100     ...
+            ,'warmup'     ,1000    ...
+            ,'thin'       ,10      ...
+            ,'working_dir','./output');
+
 fitRL.verbose = false;
 %fitRL.check();% show progress
 fitRL.block();% block further instructions
-
 toc
-save('fitRL.mat',fitRL)
 
-plot_stan_flat_rl_recovery(fitRL)
+save('./output/fitRL.mat','fitRL')
+
+plot_stan_flat_rl_recovery(fitRL,0)
 end
